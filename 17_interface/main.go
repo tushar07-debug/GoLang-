92f0495202044f8671a8a916d8e68c473b071f76
tushar07-debug <tusharsingh102003@gmail.com>
2025-02-03 21:11:02 +0530

    interface stripe

--- 17_interface/main.go
+++ 17_interface/main.go
@@ -6,8 +6,10 @@ type payment struct {
 }
 
 func (p *payment) makepayment(amount float32) {
-	razorpaypayment := razorpay{}
-	razorpaypayment.pay(amount)
+	// razorpaypayment := razorpay{}
+	stripePayment := stripe{}
+	stripePayment.pay(amount)
+	// razorpaypayment.pay(amount)
 }
 
 type razorpay struct {
@@ -16,6 +18,14 @@ type razorpay struct {
 func (r razorpay) pay(amount float32) {
 	//logic to make payment
 	fmt.Println("making payment using razorpay",amount)
+}
+
+type stripe struct{
+
+}
+
+func (s stripe) pay(amount float32) {
+	fmt.Println("making payment using stripe",amount)
 }
 func main() {
 	newPayment:=payment{}
