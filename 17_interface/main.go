aae4fc39a13177ba2f5c424d914b813c5fb18af5
tushar07-debug <tusharsingh102003@gmail.com>
2025-02-03 21:36:41 +0530

    interface  GW

--- 17_interface/main.go
+++ 17_interface/main.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
-type payment struct {
+// interface are contract
+
+type paymenter interface {
+	pay(amount float32)
 }
 
+type payment struct {
+	// gateway razorpay
+	gateway paymenter
+}
+
+//open close principle - violits
+
+//concrete implemenetation of razor pay and stripe
 func (p *payment) makepayment(amount float32) {
 	// razorpaypayment := razorpay{}
-	stripePayment := stripe{}
-	stripePayment.pay(amount)
+	// stripePayment := stripe{}
+	// stripePayment.pay(amount)
 	// razorpaypayment.pay(amount)
+	p.gateway.pay(amount)
 }
 
 type razorpay struct {
@@ -17,17 +29,36 @@ type razorpay struct {
 
 func (r razorpay) pay(amount float32) {
 	//logic to make payment
-	fmt.Println("making payment using razorpay",amount)
+	fmt.Println("making payment using razorpay", amount)
 }
 
-type stripe struct{
+// type stripe struct {
+// }
 
+// func (s stripe) pay(amount float32) {
+// 	fmt.Println("making payment using stripe", amount)
+// }
+type fakepayment struct {
 }
 
-func (s stripe) pay(amount float32) {
-	fmt.Println("making payment using stripe",amount)
+func (f fakepayment) pay(amount float32) {
+	fmt.Println("making payment using fakepayment")
+}
+
+type paypal struct {
+}
+
+func (p paypal) pay(amount float32) {
+	fmt.Println("making payment using paypal", amount)
 }
 func main() {
-	newPayment:=payment{}
+	// stripePayment := stripe{}
+	// razorpayPayment := razorpay{}
+	// fakeGW := fakepayment{}
+	paypals:= paypal{}
+	newPayment := payment{
+		// newPayment.makepayment(1000)
+		gateway: paypals,
+	}
 	newPayment.makepayment(1000)
 }
