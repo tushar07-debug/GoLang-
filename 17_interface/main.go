259071f09501b1c7c9f3efbacc44ad53925b655e
tushar07-debug <tusharsingh102003@gmail.com>
2025-02-03 21:09:04 +0530

    interface razorpay

--- 17_interface/main.go
+++ 17_interface/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+type payment struct {
+}
+
+func (p *payment) makepayment(amount float32) {
+	razorpaypayment := razorpay{}
+	razorpaypayment.pay(amount)
+}
+
+type razorpay struct {
+}
+
+func (r razorpay) pay(amount float32) {
+	//logic to make payment
+	fmt.Println("making payment using razorpay",amount)
+}
+func main() {
+	newPayment:=payment{}
+	newPayment.makepayment(1000)
 }
